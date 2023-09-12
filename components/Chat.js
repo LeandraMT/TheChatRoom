9b0c6d66d0d4ec3a5b16ea03391bb86798918b24
LeandraMT <magantierleandra@gmail.com>
2023-09-12 03:20:05 +0200

    added GiftedChat library

--- components/Chat.js
+++ components/Chat.js
@@ -1,29 +1,47 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 const ChatScreen = ({ route, navigation }) => {
-    const { name, color } = route.params;
+    const { name } = route.params;
+    const [messages, setMessages] = useState([]);
+    const onSend = (newMessages) => {
+        setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+    }
 
     useEffect(() => {
-        navigation.setOptions({ title: name });
+        navigation.setOptions({ title: name })
+    }, []);
+
+    useEffect(() => {
+        setMessages([
+            {
+                _id: 1,
+                text: 'Hello Developer',
+                createdAt: new Date(),
+                user: {
+                    _id: 2,
+                    name: 'React Native',
+                    avatar: 'https://placeimg.com/140/140/any',
+                },
+            },
+        ]);
     }, []);
 
     return (
-        <View style={[styles.container, { backgroundColor: color }]}>
-            <Text style={styles.textChat}>Hello {name}!</Text>
+        <View style={styles.container}>
+            <GiftedChat
+                messages={messages}
+                onSend={messages => onSend(messages)}
+                user={{ _id: 1 }}
+            />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    textChat: {
-        fontSize: 18,
-        fontWeight: '400'
+        flex: 1
     }
 });
 
